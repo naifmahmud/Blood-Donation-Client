658af1e4c0dc6c717fb70919348f51c507fc155a
naif <naifmahmud777@gmail.com>
2025-12-15 21:15:53 +0600

    Added firebase and add  Context

---
+++ src/Contexts/AuthProvider.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from '../AuthContext/AuthContext';
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import {auth} from '../../firebase/firebase.config'
+
+
+
+const AuthProvider = ({children}) => {
+
+    const [user,setUser]=useState([]);
+    const [loading,setLoading]=useState(true);
+
+
+    const createUserWithEmail=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const signInWithEmail=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const signOutUser=()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+    // console.log(user);
+    
+
+    useEffect(()=>{
+        const unsubscribe= onAuthStateChanged(auth,(currenUser)=>{
+            setUser(currenUser);
+            setLoading(false);
+        })
+        return (()=>
+            unsubscribe());
+    },[])
+
+
+    const userInfo={
+        createUserWithEmail,
+        signInWithEmail,
+        signOutUser,
+        user,
+        setUser,
+        loading,
+        setLoading
+    }
+    return (
+        <AuthContext value={userInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Pages/Authentication/Register.jsx
@@ -0,0 +1,7 @@
+// name
+// avatar(use imageBB to upload the user avatar)
+// blood group(a selector with option A+, A-, B+, B-, AB+, AB-, O+, O-)
+// district(select option)
+// upazila(select option)
+// password
+// confirm_password
